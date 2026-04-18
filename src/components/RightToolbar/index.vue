d18532941497e44e52de8ed7a41aea723d73f4d8
马海军 <rolkey@gmail.com>
2026-04-19 02:23:16 +0800

    format

--- src/components/RightToolbar/index.vue
+++ src/components/RightToolbar/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="top-right-btn" :style="style">
     <el-row>
-      <el-tooltip v-if="search" class="item" effect="dark" :content="showSearch ? '隐藏搜索' : '显示搜索'" placement="top">
+      <el-tooltip
+        v-if="search"
+        class="item"
+        effect="dark"
+        :content="showSearch ? '隐藏搜索' : '显示搜索'"
+        placement="top"
+      >
         <el-button circle icon="Search" @click="toggleSearch()" />
       </el-tooltip>
       <el-tooltip class="item" effect="dark" content="刷新" placement="top">

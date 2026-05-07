3b428556b7003eb758320b7923b3877ef9706f27
马海军 <rolkey@gmail.com>
2026-05-07 11:12:01 +0800

    字段长度

--- src/views/tool/gen/index.vue
+++ src/views/tool/gen/index.vue
@@ -110,13 +110,19 @@
             <span>{{ (queryParams.pageNum - 1) * queryParams.pageSize + scope.$index + 1 }}</span>
           </template>
         </el-table-column>
-        <el-table-column label="数据源" align="center" prop="dataName" :show-overflow-tooltip="true" />
+        <el-table-column
+          label="数据源"
+          align="center"
+          prop="dataName"
+          :show-overflow-tooltip="true"
+          width="120"
+        />
         <el-table-column label="表名称" align="center" prop="tableName" :show-overflow-tooltip="true" />
         <el-table-column label="表描述" align="center" prop="tableComment" :show-overflow-tooltip="true" />
         <el-table-column label="实体" align="center" prop="className" :show-overflow-tooltip="true" />
         <el-table-column label="创建时间" align="center" prop="createTime" width="160" />
         <el-table-column label="更新时间" align="center" prop="updateTime" width="160" />
-        <el-table-column label="操作" align="center" width="330" class-name="small-padding fixed-width">
+        <el-table-column label="操作" align="center" width="160" class-name="small-padding fixed-width">
           <template #default="scope">
             <el-tooltip content="预览" placement="top">
               <el-button
